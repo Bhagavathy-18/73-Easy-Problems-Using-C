e6653a206aa3ee2638bd8775eb0e9d10ca56dc49
Bhagavathy-18 <bhagavathysankar18@gmail.com>
2025-07-29 20:58:04 +0530

    Update Q12_largest_numbers.c

--- Q12_largest_numbers.c
+++ Q12_largest_numbers.c
@@ -4,14 +4,14 @@ int main() {
     float num1, num2;
 
     printf("Enter two numbers: ");
-    scanf("%f%f", &num1, &num2);
+    scanf("%f %f", &num1, &num2);
 
     if (num1 > num2) {
         printf("First number is bigger.\n");
     } else if (num1 < num2) {
         printf("Second number is bigger.\n");
     } else {
-        printf("The numbers are equal.");
+        printf("The numbers are equal.\n");
     }
 
     return 0;
