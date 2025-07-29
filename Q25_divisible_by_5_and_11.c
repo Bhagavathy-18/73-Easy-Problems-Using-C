0cef2c36db944e632b0c22f6a7c7038c9e568973
Bhagavathy-18 <bhagavathysankar18@gmail.com>
2025-07-29 21:02:43 +0530

    Update Q25_divisible_by_5_and_11.c

--- Q25_divisible_by_5_and_11.c
+++ Q25_divisible_by_5_and_11.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h> 
+bool checkDivisibility(int number) {
+    if (number % 5 == 0 && number % 11 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 int main() {
     int num;
@@ -6,7 +14,7 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if (num % 5 ==0 && num % 11 == 0) {
+    if (checkDivisibility(num)) { // Call the function to check
         printf("%d is divisible by 5 and 11.\n", num);
     } else {
         printf("%d is not divisible by 5 and 11.\n", num);
