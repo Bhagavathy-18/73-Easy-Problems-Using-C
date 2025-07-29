d22da8651f75025afc75a7a240d7aa8e0acff4bb
Bhagavathy-18 <bhagavathysankar18@gmail.com>
2025-07-29 20:54:43 +0530

    Update Q11_Avg_of_numbers.c

--- Q11_Avg_of_numbers.c
+++ Q11_Avg_of_numbers.c
@@ -4,11 +4,11 @@ int main() {
     float num1, num2, num3, average;
 
     printf("Enter three numbers: ");
-    scanf("%f%f%f", &num1, &num2, &num3);
+    scanf("%f %f %f", &num1, &num2, &num3);
 
     average = (num1 + num2 + num3) / 3;
 
-    printf("The average is %.2f.\n", average);
+    printf("The average is %.2f\n", average);
 
     return 0;
 }
